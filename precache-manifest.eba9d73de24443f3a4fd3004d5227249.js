cf83b2534aa6336aa33c07336d214df42ca29fde
Anuragsingh691 <anuragshishodia691.gzb@gmail.com>
2021-03-10 09:46:11 +0530

    Updates

---
+++ precache-manifest.eba9d73de24443f3a4fd3004d5227249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b4475146663d9612b74344a3d75e1f",
+    "url": "/Unsplash1/index.html"
+  },
+  {
+    "revision": "40c354be4cfdc9b7fc9a",
+    "url": "/Unsplash1/static/css/main.d499aabd.chunk.css"
+  },
+  {
+    "revision": "056cd0812e31728e1f47",
+    "url": "/Unsplash1/static/js/2.a96be2a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Unsplash1/static/js/2.a96be2a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40c354be4cfdc9b7fc9a",
+    "url": "/Unsplash1/static/js/main.a1281749.chunk.js"
+  },
+  {
+    "revision": "d145606b39a51f90cede",
+    "url": "/Unsplash1/static/js/runtime-main.9ae7db8a.js"
+  }
+]);
